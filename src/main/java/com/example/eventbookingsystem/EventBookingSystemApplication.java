d70b15a9b6320bfa7f3b198526a6eef004048b99
it25102411 <it25102411@my.sliit.lk>
2026-05-18 02:59:47 -0700

    Home controller,application properties,main

--- src/main/java/com/example/eventbookingsystem/EventBookingSystemApplication.java
+++ src/main/java/com/example/eventbookingsystem/EventBookingSystemApplication.java
@@ -2,12 +2,38 @@ package com.example.eventbookingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import com.example.eventbookingsystem.servlet.VenueServlet;
+import com.example.eventbookingsystem.servlet.SeatServlet;
 
 @SpringBootApplication
-public class EventBookingSystemApplication {
+public class EventBookingSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EventBookingSystemApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventBookingSystemApplication.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean<VenueServlet> venueServlet() {
+        ServletRegistrationBean<VenueServlet> bean =
+                new ServletRegistrationBean<>(new VenueServlet(), "/venue");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<SeatServlet> seatServlet() {
+        ServletRegistrationBean<SeatServlet> bean =
+                new ServletRegistrationBean<>(new SeatServlet(), "/seat");
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
 }
